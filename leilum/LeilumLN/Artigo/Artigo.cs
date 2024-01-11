dc4eaf91057039a728f3d452cd16c958ccafeaf3
Mike Pinto <mike.pinto1507@gmail.com>
2024-01-11 16:52:12 +0000

    Adding .csproj to gitignore

--- leilum/LeilumLN/Artigo/Artigo.cs
+++ leilum/LeilumLN/Artigo/Artigo.cs
@@ -12,7 +12,14 @@ namespace Leilum.LeilumLN.Artigo {
         private int lote_id;
     }
 
-
+    public Artigo(int idArtigo, string designacao, string caracteristicas, string descricao, int loteId)
+    {
+        this.id_artigo = idArtigo;
+        this.designacao = designacao;
+        this.caracteristicas = caracteristicas;
+        this.descricao = descricao;
+        this.lote_id = loteId;
+    }
 
 
 }
