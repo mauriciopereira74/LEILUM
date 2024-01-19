a12cc8c1dcfea2f2b1fafae884159bc9eb9cc6ce
mauriciopereira74 <mauriciovianense@hotmail.com>
2024-01-19 10:21:59 +0000

    fix

--- leilum/Features/Home/Card.cs
+++
@@ -1,8 +0,0 @@
-namespace leilum.Features.Home;
-public class Card
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Description { get; set; } = "";
-    public string Image { get; set; } = "";
-}
